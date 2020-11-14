e5e7a3b574a3c1534526ca956b500c0ea0330f0a
fbamuse <fukuhara.masanori@gmail.com>
2020-11-14 11:29:44 +0900

    a

---
+++ docker_upload.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="bamuse/udacity_devops_capstone"
+echo $dockerpath
+
+# Step 2:  
+# Authenticate & tag
+docker login  --username "bamuse" &&\
+docker image tag original_alpine_nginx  $dockerpath
+
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
