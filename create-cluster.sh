545eb8b34e827fd668b0e46959d564ccd33284c3
masanori <fukuhara.masanori@gmail.com>
2020-11-16 21:33:29 +0900

    stage Create kube config file

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,8 +4,8 @@ eksctl create cluster \
 --name bamuse \
 --version 1.18 \
 --region ap-northeast-1 \
---node-type t2.micro
---nodes 1 \
+--node-type t2.micro \
+--nodes 2 \
 --nodes-min 1 \
 --nodes-max 4 \
 --managed
